4d78bd01493d18399afe032ef1f9f4fc8f72e7ce
AbirShafin <abdullah.al.abirshafin@g.bracu.ac.bd>
2026-01-17 08:37:40 +0600

    wrong approach to problem 88

--- src/ArrayProblems/problem88.java
+++ src/ArrayProblems/problem88.java
@@ -1,4 +1,20 @@
 package ArrayProblems;
 
 public class problem88 {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p = 0, q = 0;
+        for(int i=0;i<m+n;i++){
+            if(nums1[p]==0){
+                nums1[p] = nums2[q++];
+            }
+            else if(nums1[p]<=nums2[q]){
+                p++;
+            }
+            else if(nums1[p]>nums2[q]){
+                int temp = nums1[p];
+                nums1[p] = nums2[q];
+                nums2[q] = temp;
+            }
+        }
+    }
 }
