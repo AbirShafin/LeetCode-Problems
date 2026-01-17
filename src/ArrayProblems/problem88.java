5618d025027b2c75ecea8a221aa9819467b97cfa
AbirShafin <abdullah.al.abirshafin@g.bracu.ac.bd>
2026-01-17 08:58:14 +0600

    correct approach to problem 88

--- src/ArrayProblems/problem88.java
+++ src/ArrayProblems/problem88.java
@@ -1,20 +1,17 @@
 package ArrayProblems;
-
-public class problem88 {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p = 0, q = 0;
-        for(int i=0;i<m+n;i++){
-            if(nums1[p]==0){
-                nums1[p] = nums2[q++];
-            }
-            else if(nums1[p]<=nums2[q]){
-                p++;
-            }
-            else if(nums1[p]>nums2[q]){
-                int temp = nums1[p];
-                nums1[p] = nums2[q];
-                nums2[q] = temp;
-            }
+public class problem88{
+public void merge(int[] nums1, int m, int[] nums2, int n) {
+    int p = m-1, q = n-1, k = nums1.length-1;
+    while(p>=0 && q>=0){
+        if(nums1[p]>=nums2[q]){
+            nums1[k--] = nums1[p--];
+        }
+        else if(nums1[p]<nums2[q]){
+            nums1[k--] = nums2[q--];
         }
     }
+    while(q>=0){
+        nums1[k--]=nums2[q--];
+    }
+}
 }
